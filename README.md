b305be1dd1458752003bc7053dbe27c42c0dc6ee
Dan Nguyen <dansonguyen@gmail.com>
2015-08-01 22:27:14 -0700

    some congress data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tutorial-data-stash
+
+The raw data and scripts to collect them, as used and mirrored for Stanford Computational Journalism data tutorials at http://tutorials.compjour.org.
---
+++ scripts/congress_votes/get_votes.sh
@@ -0,0 +1,8 @@
+# Pull roll call vote data via govtrack.us rsync server
+# https://www.govtrack.us/developers/data
+# run this from root of project, e.g.
+# bash scripts/congress_votes/get_votes.sh
+wkdir=./datahaus/congress/votes/downloaded/govtrack
+mkdir -p $wkdir
+rsync -avz --delete --delete-excluded --exclude **/text-versions/ \
+    govtrack.us::govtrackdata/congress/114/votes/ $wkdir
